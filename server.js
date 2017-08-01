57c47b5de13b4b665dc54d2a768a20735082846a
Jack Tanner <ugmo04@users.noreply.github.com>
2017-08-01 18:23:11 +0100

    updated files for deployment to elastic beanstalk

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080);
+app.listen(port);
 console.log("App listening on port 8080");
